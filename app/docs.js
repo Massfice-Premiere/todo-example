b84c53347fe7ff574c33a0c0cb1976a052fd8a4e
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-04-16 15:08:16 +0200

    removing swagger host

--- app/docs.js
+++ app/docs.js
@@ -2,7 +2,6 @@ const docs = require('../config/docs.json');
 const components = require('../config/components.json');
 const paths = require('../config/paths.json');
 const { version, description } = require('../package.json');
-const { swagger: { host } } = require('../config');
 
 module.exports = {
     ...docs,
@@ -11,7 +10,6 @@ module.exports = {
         version,
         description
     },
-    host,
     components,
     paths
 };
--- config/index.js
+++ config/index.js
@@ -4,9 +4,6 @@ module.exports = {
     app: {
         port
     },
-    swagger: {
-        host: process.env.APPLICATION_HOST || `localhost:${port}`
-    },
     mongo: {
         uri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017',
         dbname: process.env.MONGO_DBNAME || 'todos'
